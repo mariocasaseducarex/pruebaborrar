a79f15bca93f1bc689daca8da4b380c4dfb2b790
mcasas <mcasase01@educarex.es>
2025-10-24 12:22:24 +0200

    primer commit

---
+++ src/proyectoGit/Saludo.java
@@ -0,0 +1,21 @@
+package proyectoGit;
+
+import java.util.Scanner;
+
+public class Saludo {
+    public static void main(String[] args) {
+        // Crear un objeto Scanner para leer del teclado
+        Scanner teclado = new Scanner(System.in);
+
+        // Pedir el nombre al usuario
+        System.out.print("Introduce tu nombre: ");
+        String nombre = teclado.nextLine();
+       
+
+        // Mostrar el saludo
+        System.out.println("¡Hola, " + nombre + "! Bienvenido al programa.");
+
+        // Cerrar el Scanner
+        teclado.close();
+    }
+}
